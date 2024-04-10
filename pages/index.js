85f8fe56104e9e7e15aaee86931b6b1876043bb3
Michael Sweeney <mike@nclusion.com>
2024-04-09 17:59:20 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -6,18 +6,13 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>diadesabor.com</title>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+        <h1>Coming soon: All your salspectations!</h1>
       </main>
 
-      <Footer />
     </div>
   )
 }
