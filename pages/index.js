5882ff7f5dd48b13001f4eaaf8bcf366d8d8974a
Michael Sweeney <mike@nclusion.com>
2024-04-14 10:47:35 -0700

    added styling

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,16 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>diadesabor.com</title>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
       </Head>
 
       <main>
-        <h1>Coming soon: All your salspectations!</h1>
+        <img src="/salsas.jpg" alt="salsa" />
+        <div className="hero-content">
+          <h1>Coming soon: All your salspectations!</h1>
+        </div>
       </main>
 
     </div>
